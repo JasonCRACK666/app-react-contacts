37935910fb1170641da5c85ccfa02418d190ea14
JasonCRACK666 <emerzonkolki@gmail.com>
2022-05-29 12:46:38 -0500

    Changes for the contacts page and adding the contact page completed

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const ContactItem = ({ last_name, name, nickname, phone_number, id }) => {
   return (
     <Link
-      to={`/contacts/${id}`}
+      to={`/contact/${id}`}
       className="p-4 bg-gray-800 hover:bg-gray-700 rounded-md cursor-pointer transition-colors"
     >
       <h4 className="text-xl font-semibold text-green-500">{nickname}</h4>
