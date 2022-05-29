753dc3c28fbf9162db2d57bdcf41dc080bf1259e
JasonCRACK666 <emerzonkolki@gmail.com>
2022-05-28 23:39:50 -0500

    Añadiendo la pagina de los contactos del usuario y 2 componentes importantes para el layout principal

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,29 @@
+import LayoutMain from "../components/layouts/LayoutMain";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getContacts } from "../services/contactsServices";
+import { setContacts } from "../redux/slices/contactsSlice";
+
 const Contacts = () => {
-  return <div>Contacts</div>;
+  const dispatch = useDispatch();
+  const contacts = useSelector((state) => state.contacts.contacts);
+  const token = localStorage.getItem("token");
+
+  const setContactsRedux = async (token) => {
+    const contacts = await getContacts(token);
+    dispatch(setContacts(contacts));
+  };
+
+  useEffect(() => {
+    setContactsRedux(token);
+  }, [token]);
+
+  return (
+    <LayoutMain>
+      {/* // TODO: hacer un mapeo de los contactos */}
+      <div className="grid"></div>
+    </LayoutMain>
+  );
 };
 
 export default Contacts;
